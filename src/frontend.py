9a3a5ea929da4c02926aca96ffdd752a04c7d7ec
Cristian Guilarte <crislgf1@gmail.com>
2023-10-29 17:45:36 +0000

    Support for iteractive selection

--- src/frontend.py
+++ src/frontend.py
@@ -2,6 +2,8 @@ import customtkinter as ctk
 import tkinter as tk
 from tkinter import ttk
 
+import backend as bd
+
 
 class MainFrame(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
@@ -36,14 +38,14 @@ class Window(ctk.CTk):
         self.frame_desc.grid(row=4, column=0, padx=10, pady=10, sticky="nsew")
 
         self.button_add = ctk.CTkButton(
-                self.frame_main, text="Add Product", command=self.add_product
-                )
+            self.frame_main, text="Add Product", command=self.add_product
+        )
         self.button_modify = ctk.CTkButton(
-                self.frame_main, text="Modify Product", command=self.modify_product
-                )
+            self.frame_main, text="Modify Product", command=self.modify_product
+        )
         self.button_delete = ctk.CTkButton(
-                self.frame_main, text="Delete Product", command=self.delete_product
-                )
+            self.frame_main, text="Delete Product", command=self.delete_product
+        )
         self.button_add.grid(row=0, column=0, padx=5, pady=10)
         self.button_modify.grid(row=1, column=0, padx=5, pady=10)
         self.button_delete.grid(row=2, column=0, padx=5, pady=10)
@@ -53,22 +55,28 @@ class Window(ctk.CTk):
         self.lable_price.grid(row=0, column=0, padx=5, pady=10)
         self.lable_desc.grid(row=2, column=0, padx=5, pady=10)
 
-        self.strVar_price = ctk.StringVar(value="0.0")
-        self.strVar_desc = ctk.StringVar(value="Product without information.")
+        # self.strVar_price = ctk.StringVar(value="0.0")
+        # self.strVar_desc = ctk.StringVar(value="Product without information.")
         self.lable_show_price = ctk.CTkLabel(
-                self.frame_price, text=self.strVar_price.get()
-                )
-        self.lable_show_desc = ctk.CTkLabel(self.frame_desc, text=self.strVar_desc.get())
+            self.frame_price, text="0.0$"
+        )
+        self.lable_show_desc = ctk.CTkLabel(
+            self.frame_desc, text="No information"
+        )
         self.lable_show_price.grid(row=1, column=0, padx=5, pady=10)
         self.lable_show_desc.grid(row=3, column=0, padx=5, pady=10)
 
-        self.table_columns = ("name", "brand", "stock")
+        self.table_columns = ("name", "desc", "price", "brand", "stock")
         self.table = ttk.Treeview(self, show="headings", columns=self.table_columns)
         self.table.heading("name", text="Name")
+        self.table.heading("desc", text="Description")
+        self.table.heading("price", text="Price")
         self.table.heading("brand", text="Brand")
         self.table.heading("stock", text="Stock")
         self.table.bind("<<TreeviewSelect>>", self.get_product_details)
         self.table.grid(row=0, column=0, padx=20, pady=20)
+
+        self.load_items()
 
     def delete_product(self):
         print("Product Deleted [DEMO]")
@@ -79,11 +87,14 @@ class Window(ctk.CTk):
     def add_product(self):
         print("Product Added [DEMO]")
 
-    def get_product_desc(self):
-        ...
+    def load_items(self, *args, **kwargs):
+        print(f"Args: {args}")
+        print(f"Kwargs: {kwargs}")
+        items = bd.get_product_details()
+        for item in items:
+            self.table.insert("", "end", values=item)  # [0], item[3], item[4]))
 
-    def get_product_price(self):
-        ...
-
-    def get_product_details(self):
-        ...
+    def get_product_details(self, *args):
+        select_item = self.table.item(self.table.selection())
+        self.lable_show_price.configure(text=f"${select_item['values'][2]}")
+        self.lable_show_desc.configure(text=select_item["values"][1])
